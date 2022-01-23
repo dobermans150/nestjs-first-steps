86528b03f2dcd5147829d6056cd9765c7a83e6a5
Christian <chrisdb150@gmail.com>
2022-01-23 10:59:00 -0400

    creating tasks modules

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,27 +1,47 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  /*  Res,
+  Req, */
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/createTask.dto';
+/* import { Request, Response } from 'express'; */
 
 @Controller('tasks')
 export class TasksController {
   @Get()
-  getTasks(): string {
-    return 'retornando tareas';
+  getTasks(): { hello: string } {
+    return { hello: 'world' };
   }
+  /*
+    Manera de hacerlo con Express
+
+  @Get()
+  getTasks(@Req() req, @Res() res): Response {
+    return res.send('Hellos world');
+  } */
 
   @Post()
   createTask0(@Body() task: CreateTaskDto): string {
-    console.log(task);
-
     return 'creating a task';
   }
 
-  @Put()
-  updateTask() {
+  @Put('/:id')
+  updateTask(@Body() task: CreateTaskDto, @Param() id: string) {
+    console.log(task, id);
+
     return 'updating a task';
   }
 
-  @Delete()
-  deleteTask(): string {
-    return 'Deleting a task';
+  @Delete('/:id')
+  deleteTask(@Param() id): string {
+    console.log(id);
+
+    return `Deleting a task ${id}`;
   }
 }
